30479c2f7469953dd324b100a7613ffe0cfd1fb8
Kecin2019 <1139962358@qq.com>
2023-12-25 16:22:16 +0800

    更新

--- scripts/shapenet_airplane_demo.sh
+++ scripts/shapenet_airplane_demo.sh
@@ -6,6 +6,6 @@ python demo.py \
     --dims 512-512-512 \
     --latent_dims 256-256 \
     --use_latent_flow \
-    --num_sample_shapes 3 \
+    --num_sample_shapes 10 \
     --num_sample_points 50000
 
