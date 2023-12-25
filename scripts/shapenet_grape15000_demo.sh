aabd0e01a19881fc1df3d1ccbe19ce769b37503d
Kecin2019 <1139962358@qq.com>
2023-12-25 11:17:35 +0800

    更新相关参数

--- scripts/shapenet_grape15000_demo.sh
+++ scripts/shapenet_grape15000_demo.sh
@@ -2,7 +2,7 @@
 
 python demo.py \
     --cates grape15000 \
-    --resume_checkpoint pretrained_models/gen/airplane/checkpoint.pt \
+    --resume_checkpoint checkpoints/gen/shapenet15k-categrape15000/checkpoint-latest.pt \
     --dims 512-512-512 \
     --latent_dims 256-256 \
     --use_latent_flow \
