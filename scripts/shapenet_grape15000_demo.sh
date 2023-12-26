a1d12ea779dc850d860af15930f442e3597eb0e9
Kecin2019 <1139962358@qq.com>
2023-12-26 21:19:57 +0800

    更改输出时的模型输入以及添加实验文件

--- scripts/shapenet_grape15000_demo.sh
+++ scripts/shapenet_grape15000_demo.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-python demo.py \
+python demo_grape15000.py \
     --cates grape15000 \
-    --resume_checkpoint checkpoints/gen/shapenet15k-categrape15000/checkpoint-latest.pt \
+    --resume_checkpoint checkpoints/gen/tmp/shapenet15k-categrape15000/checkpoint-latest.pt \
     --dims 512-512-512 \
     --latent_dims 256-256 \
     --use_latent_flow \
-    --num_sample_shapes 3 \
+    --num_sample_shapes 10 \
     --num_sample_points 50000
 
--- scripts/shapenet_grape15000_gen.sh
+++ scripts/shapenet_grape15000_gen.sh
@@ -6,7 +6,7 @@ latent_dims="256-256"
 num_blocks=1
 latent_num_blocks=1
 zdim=128
-batch_size=32
+batch_size=4
 lr=2e-3
 epochs=4000
 ds=shapenet15k
@@ -30,6 +30,7 @@ python train.py \
     --viz_freq 10 \
     --log_freq 10 \
     --val_freq 10 \
+    --tr_max_sample_points 10000 \
     --use_latent_flow
 
 echo "Done"
